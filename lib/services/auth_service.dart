f296c3f609277ac6d395c05fe3811e3b1086aea6
jideguru <guruliciousjide@gmail.com>
2020-07-08 14:37:48 +0100

    Added firebase files to gitignore

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -3,15 +3,15 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:social_app_ui/services/services.dart';
 
 class AuthService extends Services {
-  Future<bool> checkUser(String email) async{
+  Future<bool> checkUser(String email) async {
     QuerySnapshot snap = await firestore
         .collection('users')
         .where("email", isEqualTo: email)
         .getDocuments();
 
-    if(snap.documents.isNotEmpty){
+    if (snap.documents.isNotEmpty) {
       return true;
-    }else{
+    } else {
       return false;
     }
   }
@@ -30,7 +30,7 @@ class AuthService extends Services {
         "email": user.email,
         "name": name,
         "signedUpAt": Timestamp.now(),
-      }).catchError((e){
+      }).catchError((e) {
         print(e);
       });
       return true;
@@ -39,15 +39,15 @@ class AuthService extends Services {
     }
   }
 
-  Future<bool> loginUser({String email, String password}) async{
+  Future<bool> loginUser({String email, String password}) async {
     var res = await auth.signInWithEmailAndPassword(
       email: '$email',
       password: '$password',
     );
 
-    if(res.user != null){
+    if (res.user != null) {
       return true;
-    }else{
+    } else {
       return false;
     }
   }
@@ -57,20 +57,20 @@ class AuthService extends Services {
     await auth.signOut();
   }
 
-  String handleFirebaseAuthError(String e){
-    if(e.contains("ERROR_WEAK_PASSWORD")){
+  String handleFirebaseAuthError(String e) {
+    if (e.contains("ERROR_WEAK_PASSWORD")) {
       return "Password is too weak";
-    }else if(e.contains("ERROR_INVALID_EMAIL")){
+    } else if (e.contains("ERROR_INVALID_EMAIL")) {
       return "Invalid Email";
-    }else if(e.contains("ERROR_EMAIL_ALREADY_IN_USE")){
+    } else if (e.contains("ERROR_EMAIL_ALREADY_IN_USE")) {
       return "The email address is already in use by another account.";
-    }else if(e.contains("ERROR_NETWORK_REQUEST_FAILED")){
+    } else if (e.contains("ERROR_NETWORK_REQUEST_FAILED")) {
       return "Network error occured!";
-    }else if(e.contains("ERROR_USER_NOT_FOUND")){
+    } else if (e.contains("ERROR_USER_NOT_FOUND")) {
       return "Invalid credentials.";
-    }else if(e.contains("ERROR_WRONG_PASSWORD")){
+    } else if (e.contains("ERROR_WRONG_PASSWORD")) {
       return "Invalid credentials.";
-    }else{
+    } else {
       return e;
     }
   }
