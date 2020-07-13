93558004273a4586e750627260ca3f13bd1fe58c
jideguru <guruliciousjide@gmail.com>
2020-07-14 00:15:27 +0100

    Finished up chat list

--- lib/components/chat_item.dart
+++ lib/components/chat_item.dart
@@ -1,110 +1,118 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:social_app_ui/models/user.dart';
 import 'package:social_app_ui/views/chat/conversation/conversation.dart';
+import 'package:timeago/timeago.dart' as timeago;
 
-class ChatItem extends StatefulWidget {
-  final String dp;
-  final String name;
-  final String time;
+class ChatItem extends StatelessWidget {
+  final String userID;
+  final Timestamp time;
   final String msg;
-  final bool isOnline;
   final int counter;
 
   ChatItem({
     Key key,
-    @required this.dp,
-    @required this.name,
+    @required this.userID,
     @required this.time,
     @required this.msg,
-    @required this.isOnline,
     @required this.counter,
   }) : super(key: key);
 
-  @override
-  _ChatItemState createState() => _ChatItemState();
-}
+  final Firestore firestore = Firestore.instance;
 
-class _ChatItemState extends State<ChatItem> {
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      contentPadding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 5.0),
-      leading: Stack(
-        children: <Widget>[
-          CircleAvatar(
-            backgroundImage: AssetImage(
-              "${widget.dp}",
-            ),
-            radius: 25.0,
-          ),
-          Positioned(
-            bottom: 0.0,
-            right: 0.0,
-            child: Container(
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(6.0),
-              ),
-              height: 15,
-              width: 15,
-              child: Center(
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: widget.isOnline
-                        ? Color(0xff00d72f)
-                        : Colors.grey,
-                    borderRadius: BorderRadius.circular(6),
+    return StreamBuilder(
+      stream: firestore.collection('users').document('$userID').snapshots(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          DocumentSnapshot documentSnapshot = snapshot.data;
+          User user = User.fromJson(documentSnapshot.data);
+
+          return ListTile(
+            contentPadding:
+                EdgeInsets.symmetric(horizontal: 20.0, vertical: 5.0),
+            leading: Stack(
+              children: <Widget>[
+                CircleAvatar(
+                  backgroundImage: CachedNetworkImageProvider(
+                    '${user.profilePicture}',
                   ),
-                  height: 11,
-                  width: 11,
+                  radius: 25.0,
                 ),
+                Positioned(
+                  bottom: 0.0,
+                  right: 0.0,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(6.0),
+                    ),
+                    height: 15,
+                    width: 15,
+                    child: Center(
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: user.isOnline ? Color(0xff00d72f) : Colors.grey,
+                          borderRadius: BorderRadius.circular(6),
+                        ),
+                        height: 11,
+                        width: 11,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            title: Text(
+              '${user.name}',
+              maxLines: 1,
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
               ),
             ),
-          ),
-        ],
-      ),
-      title: Text(
-        "${widget.name}",
-        maxLines: 1,
-        style: TextStyle(
-          fontWeight: FontWeight.bold,
-        ),
-      ),
-      subtitle: Text(
-        "${widget.msg}",
-        overflow: TextOverflow.ellipsis,
-        maxLines: 2,
-      ),
-      trailing: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: <Widget>[
-          SizedBox(height: 10),
-          Text(
-            "${widget.time}",
-            style: TextStyle(
-              fontWeight: FontWeight.w300,
-              fontSize: 11,
+            subtitle: Text(
+              "$msg",
+              overflow: TextOverflow.ellipsis,
+              maxLines: 2,
             ),
-          ),
-          SizedBox(height: 5),
-          buildCounter(),
-        ],
-      ),
-      onTap: () {
-        Navigator.of(context, rootNavigator: true).push(
-          MaterialPageRoute(
-            builder: (BuildContext context) {
-              return Conversation();
+            trailing: Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: <Widget>[
+                SizedBox(height: 10),
+                Text(
+                  "${timeago.format(time.toDate())}",
+                  style: TextStyle(
+                    fontWeight: FontWeight.w300,
+                    fontSize: 11,
+                  ),
+                ),
+                SizedBox(height: 5),
+                buildCounter(context),
+              ],
+            ),
+            onTap: () {
+              Navigator.of(context, rootNavigator: true).push(
+                MaterialPageRoute(
+                  builder: (BuildContext context) {
+                    return Conversation();
+                  },
+                ),
+              );
             },
-          ),
-        );
+          );
+        } else {
+          return SizedBox();
+        }
       },
     );
   }
 
-  buildCounter(){
-    if(widget.counter == 0){
+  buildCounter(BuildContext context) {
+    if (counter == 0) {
       return SizedBox();
-    }else{
+    } else {
       return Container(
         padding: EdgeInsets.all(1),
         decoration: BoxDecoration(
@@ -118,7 +126,7 @@ class _ChatItemState extends State<ChatItem> {
         child: Padding(
           padding: EdgeInsets.only(top: 1, left: 5, right: 5),
           child: Text(
-            "${widget.counter}",
+            "$counter",
             style: TextStyle(
               color: Colors.white,
               fontSize: 10,
--- lib/views/chat/chats/chats.dart
+++ lib/views/chat/chats/chats.dart
@@ -2,9 +2,9 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_icons/flutter_icons.dart';
 import 'package:social_app_ui/components/chat_item.dart';
-import 'package:social_app_ui/views/chat/new_chat/new_chat.dart';
-import 'package:social_app_ui/util/data.dart';
+import 'package:social_app_ui/models/message.dart';
 import 'package:social_app_ui/util/router.dart';
+import 'package:social_app_ui/views/chat/new_chat/new_chat.dart';
 
 class Chats extends StatelessWidget {
   final Firestore firestore = Firestore.instance;
@@ -26,12 +26,37 @@ class Chats extends StatelessWidget {
         ],
       ),
       body: StreamBuilder(
-        stream: firestore.collection("user").where("users", arrayContains: "bIIMBJbUOYO87MiKdZf4FRrAuP83").snapshots(),
+        stream: userChatsStream('bIIMBJbUOYO87MiKdZf4FRrAuP83'),
         builder: (context, snapshot) {
-          if(snapshot.hasData){
-            QuerySnapshot querySnapshot = snapshot.data;
-            List docs = querySnapshot.documents;
+          if (snapshot.hasData) {
+            List chatList = snapshot.data.documents;
             return ListView.separated(
+              itemCount: chatList.length,
+              itemBuilder: (BuildContext context, int index) {
+                DocumentSnapshot documentSnapshot = chatList[index];
+                return StreamBuilder(
+                  stream: messageListStream(documentSnapshot.documentID),
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      List messages = snapshot.data.documents;
+                      Message message = Message.fromJson(messages.first.data);
+                      List users = documentSnapshot.data['users'];
+                      // remove the current user's id from the Users
+                      // list so we can get the second user's id
+                      users.remove("bIIMBJbUOYO87MiKdZf4FRrAuP83");
+                      String recipient = users[0];
+                      return ChatItem(
+                        userID: recipient,
+                        counter: 8,
+                        msg: message.content,
+                        time: message.time,
+                      );
+                    }else{
+                      return SizedBox();
+                    }
+                  }
+                );
+              },
               separatorBuilder: (BuildContext context, int index) {
                 return Align(
                   alignment: Alignment.centerRight,
@@ -42,23 +67,11 @@ class Chats extends StatelessWidget {
                   ),
                 );
               },
-              itemCount: chats.length,
-              itemBuilder: (BuildContext context, int index) {
-                Map chat = chats[index];
-                return ChatItem(
-                  dp: chat['dp'],
-                  name: chat['name'],
-                  isOnline: chat['isOnline'],
-                  counter: chat['counter'],
-                  msg: chat['msg'],
-                  time: chat['time'],
-                );
-              },
             );
-          }else{
+          } else {
             return Center(child: CircularProgressIndicator());
           }
-        }
+        },
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(
@@ -68,5 +81,21 @@ class Chats extends StatelessWidget {
         onPressed: () => Router.pushPageDialog(context, NewChat()),
       ),
     );
+  }
+
+  Stream<QuerySnapshot> userChatsStream(String uid){
+    return firestore
+        .collection('chats')
+        .where('users', arrayContains: '$uid')
+        .snapshots();
+  }
+
+  Stream<QuerySnapshot> messageListStream(String documentId){
+    return firestore
+        .collection("chats")
+        .document(documentId)
+        .collection('messages')
+        .orderBy('time')
+        .snapshots();
   }
 }
