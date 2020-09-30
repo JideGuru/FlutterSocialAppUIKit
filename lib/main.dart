b6928ab75ed02ef7998a6f651100aa080391c7b8
mattscepter <56641434+mattscepter@users.noreply.github.com>
2020-09-30 20:51:32 +0530

    Import

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:social_app_ui/screens/splash/splash.dart';
 import 'package:social_app_ui/util/const.dart';
