424df7b063a416dd34d84c6682442afb11c68433
timilehinjegede <timilehintimothy.jegede@gmail.com>
2020-10-12 09:20:09 +0100

    chore: fixed naming conflicts

--- lib/screens/splash/splash.dart
+++ lib/screens/splash/splash.dart
@@ -1,6 +1,6 @@
 import 'dart:async';
 
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' hide Router;
 import 'package:social_app_ui/components/animations/type_write.dart';
 import 'package:social_app_ui/screens/auth/check_email.dart';
 import 'package:social_app_ui/util/const.dart';
